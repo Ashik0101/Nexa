a83fe7774491e4cc4f2dc923be0c2f3295e50203
Md Ashik Ansari <115465868+Ashik0101@users.noreply.github.com>
2024-08-25 23:19:39 +0530

    LatestCollection Section Completed

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
+import Title from "../components/Title";
 
 const Home = () => {
 	return (
 		<>
 			<Hero />
+			<LatestCollection />
 		</>
 	);
 };
