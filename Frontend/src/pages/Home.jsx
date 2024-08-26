1683c96594e107648d55c8c9bd00a34e2a223123
Md Ashik Ansari <115465868+Ashik0101@users.noreply.github.com>
2024-08-26 21:24:27 +0530

    Change Bestseller to BestSeller

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import Title from "../components/Title";
-import Bestseller from "../components/Bestseller";
+import BestSeller from "../components/BestSeller";
 
 const Home = () => {
 	return (
 		<>
 			<Hero />
 			<LatestCollection />
-			<Bestseller />
+			<BestSeller />
 		</>
 	);
 };
